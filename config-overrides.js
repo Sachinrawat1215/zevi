449ac30401dd4161107ad31116884c69f40dd338
Sachinrawat1215 <Sachinrawat0853@gmail.com>
2023-12-02 13:32:57 +0530

    resolve alias route issue

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { alias } = require('react-app-rewire-alias');
+
+module.exports = function override(config) {
+  return alias({
+    '@components': 'src/components',
+    '@utils': 'src/utils',
+  })(config);
+};
